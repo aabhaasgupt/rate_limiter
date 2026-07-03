7add3dd7c0a1e1d6c64f3bc5afac359bfd5a517a
Aabhaas Gupta <aabhaasgupta@Aabhaass-MacBook-Pro.local>
2026-07-02 23:40:25 -0700

    Initial Commit

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask;
+
+app = Flask(__name__)
+
+
+@app.get("/limit")
+def get_limit():
+    return {"decision": "allow"}
+
+@app.get("/health")
+def get_health():
+    return {"status": "healthy"}
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
+
+
+
